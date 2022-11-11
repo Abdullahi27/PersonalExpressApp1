dc801bba3db46e464d7b0a9737d776d9f156be90
Abdullahi27 <111465917+Abdullahi27@users.noreply.github.com>
2022-11-11 13:50:17 -0500

    initial commit

--- public/main.js
+++ public/main.js
@@ -1,13 +1,14 @@
-document.getElementById('test').addEventListener('click', ()=> {
+document.getElementById('search').addEventListener('click', ()=> {
   let userInput = document.querySelector('input').value
-  fetch(`/test`, {
-    method: 'post',
+  console.log('fetch/pokemon')
+  fetch(`/pokemon?name=${userInput}`, {
+    method: 'get',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({
-      'pokemon': userInput,
-    })
+    // body: JSON.stringify({
+    //   'pokemon': userInput
+    // })
   })
     .then((response) => response.json())
     .then((data) => {
@@ -24,12 +25,124 @@ document.getElementById('test').addEventListener('click', ()=> {
       <h2>Height: ${data.height}</h2>
       <h2>Moves: ${data.moves[2].move.name}</h2>
       <h2>Ability: ${data.abilities[1].ability.name}</h2>
-      <img src="${data.sprites.front_default}" alt="">
-      
+      <img id='saveimg' src="${data.sprites.front_default}" alt="">
+      <button class='save' data-name='${data.name}' data-height='${data.height}' data-ability='${data.abilities[1].ability.name}' data-img='${data.sprites.front_default}' data-moves='${data.moves[2].move.name}' > Save! </button>
       `
+     
+
       console.log(data.name)
       console.log(data.abilities[1].ability.name)
 
+     document.querySelector(".save").addEventListener('click', (e)=> {
+      let name = e.target.dataset.name
+      let height = e.target.dataset.height
+      let moves = e.target.dataset.moves
+      let ability = e.target.dataset.ability
+      let img = e.target.dataset.img
+  
+      console.log(name, height, moves, ability, img)
+
+      fetch('/savepokemon', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          'name': name,
+          'height': height,
+          'moves': moves,
+          'ability': ability,
+          'img': img
+      })
+          
+        }) .then(function (response) {
+          window.location.reload()
+        })
+      })
+     }) 
+
     })
+
+
+
+
+let deletebtn = document.querySelectorAll("#deletebtn")
+
+
+Array.from(deletebtn).forEach(function(element) {
+  element.addEventListener('click', function(){
+    console.log(element.dataset.id)
+    fetch('delete', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'id': element.dataset.id
+      })
+    }).then(function (response) {
+      window.location.reload()
+    })
+  });
+});
+
+
+let powerInput = document.querySelectorAll(".saveP");
+
+console.log(powerInput)
+Array.from(powerInput).forEach(function (element) {
+  element.addEventListener('click', function (e) {
+    let power = this.parentNode.querySelector('select').value
+    let id = element.dataset.id
+   console.log(id)
+   fetch('power', {
+    method: 'put',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'id': id,
+      'power': power
+      
+    })
+  })
+  .then(function (response) {
+    window.location.reload()
+  })
+   
+  })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const MongoClient = require('mongodb').MongoClient
 
 var db, collection;
 
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
-const dbName = "demo";
+const url = 'mongodb+srv://demo:demo123@cluster0.t5ge5m8.mongodb.net/pokemon?retryWrites=true&w=majority';
+const dbName = "pokemon";
 
 app.listen(3000, () => {
     console.log('app is running on port 3000')
@@ -24,13 +24,22 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.render('index.ejs')
-})
+app.get('/', function(req, res) {
+  console.log('get /pokemon')
+    db.collection('pokemon').find().toArray((err, result) => {
+      if (err) return console.log(err)
+      res.render('index.ejs', {
+        pokemon: result
+      })
+    })
+  }); 
 
+  const ObjectId = require('mongodb').ObjectID;
 
-app.post('/test', (req, res) => {
-fetch(`https://pokeapi.co/api/v2/pokemon/${req.body.pokemon}`)
+app.get('/pokemon', (req, res) => {
+  const pokename = req.query.name
+  console.log(pokename)
+fetch(`https://pokeapi.co/api/v2/pokemon/${pokename}`)
 .then(response => response.json())
 .then(data => {
    console.log(data)
@@ -38,4 +47,39 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${req.body.pokemon}`)
  
     
 }).catch(error => res.end(JSON.stringify('false')))
+
+
 })
+
+app.post('/savepokemon', (req, res) => {
+  db.collection('pokemon').save({name: req.body.name, height: req.body.height, moves: req.body.moves, ability: req.body.ability, img: req.body.img, id: new ObjectId(), power: 0}, (err, result) => {
+    console.log(req.body.name)
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.delete('/delete', (req, res) => {
+    db.collection('pokemon').findOneAndDelete({_id: ObjectId(req.body.id)}, (err, result) => {
+      if (err) return res.send(500, err)
+      res.send('Message deleted!')
+    })
+  })
+
+
+
+  app.put('/power', (req, res) => {
+    db.collection('pokemon')
+    .findOneAndUpdate({_id: ObjectId(req.body.id)}, {
+      $set: {
+        power: req.body.power
+      }
+    }, {
+      sort: {_id: -1},
+      upsert: true
+    }, (err, result) => {
+      if (err) return res.send(err)
+      res.send(result)
+    })
+  })
